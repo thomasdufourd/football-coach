cbaf8e632ac0fa34458fb437315392a9d2f3b831
Thomas Dufourd <thomas.dufourd@gmail.com>
2020-05-05 00:05:04 +0200

    Prototyping teams w/ dragndrop

---
+++ src/mocking/TeamsMockdata.ts
@@ -0,0 +1,73 @@
+import {__2_3_1, __3_3_2, __4_3_1, __7er, __9er, Lineup, Role,} from "../domain/PlayerUtils";
+import {CandidatePlayer} from "../teams/Teams";
+
+// Lineups G2008
+/*export const teamG2008Lag1: CandidatePlayer[] = [
+        {playerName: 'Tobias', },
+        {playerName: 'Sivert', role: Role.LEFT_BACK},
+        {playerName: 'Juster', role: Role.CENTRE_BACK_LEFT},
+        {playerName: 'Storm', role: Role.CENTRE_BACK_RIGHT},
+        {playerName: 'August B', role: Role.RIGHT_BACK},
+        {playerName: 'Lukas', role: Role.LEFT_WING},
+        {playerName: 'Theo', role: Role.BOX_TO_BOX_MIDFIELD},
+        {playerName: 'Dominic', role: Role.RIGHT_WING},
+        {playerName: 'Patricio', role: Role.STRIKER}
+    ],
+    substitutes: [
+        'Nedim', 'Max', 'Amund SF'
+    ]
+};*/
+
+export const lineupG2008Lag2: Lineup = {
+    team: {
+        group: {
+            name: "G2008",
+            yearOfBirth: 2008, gender: "boy", players: [] // TODO: populate with all players
+        },
+        name: "Lag #2",
+        order: 2,
+        players: [],
+        tacticalSchemaType: __9er
+    },
+    schema: __3_3_2,
+    starting: [
+        {playerName: 'Mats', role: Role.GOAL_KEEPER},
+        {playerName: 'Amund SA', role: Role.LEFT_WING},
+        {playerName: 'Max', role: Role.CENTRE_BACK_LEFT},
+        {playerName: 'Martin', role: Role.CENTRE_BACK_RIGHT},
+        {playerName: 'Mahdi', role: Role.RIGHT_WING},
+        {playerName: 'Aleksa', role: Role.DEFENSIVE_MIDFIELD},
+        {playerName: 'Fredrik', role: Role.LEFT_MIDFIELD},
+        {playerName: 'Aleks', role: Role.OFFENSIVE_MIDFIELD},
+        {playerName: 'Joel', role: Role.RIGHT_MIDFIELD}
+    ],
+    substitutes: [
+        'Daniel', 'Jonathan', 'August DN'
+    ]
+};
+
+export const lineupG2008Lag3: Lineup = {
+    team: {
+        group: {
+            name: "G2008",
+            yearOfBirth: 2008, gender: "boy", players: [] // TODO: populate with all players
+        },
+        name: "Lag #3",
+        order: 3,
+        players: [],
+        tacticalSchemaType: __7er
+    },
+    schema: __2_3_1,
+    starting: [
+        {playerName: 'Mats', role: Role.GOAL_KEEPER},
+        {playerName: 'Max', role: Role.CENTRE_BACK_LEFT},
+        {playerName: 'Behez', role: Role.CENTRE_BACK_RIGHT},
+        {playerName: 'Joel', role: Role.LEFT_MIDFIELD},
+        {playerName: 'Tobias', role: Role.BOX_TO_BOX_MIDFIELD},
+        {playerName: 'Ali', role: Role.RIGHT_MIDFIELD},
+        {playerName: 'Sufyan', role: Role.STRIKER}
+    ],
+    substitutes: [
+        'Gabriel', 'Ludvik'
+    ]
+};
--- src/teams/Teams.tsx
+++ src/teams/Teams.tsx
@@ -1,11 +1,106 @@
 import * as React from 'react';
+import {useState} from 'react';
 import {Breadcrumb, Button, Col, Container, Row} from "react-bootstrap";
+import {DragDropContext, Draggable, Droppable} from 'react-beautiful-dnd';
 
 interface Props {
     group: string;
 }
 
+export interface CandidatePlayer {
+    id: string;
+    playerName: string;
+}
+
+
+// fake data generator
+const getCandidatePlayers = (count: number, offset = 0): CandidatePlayer[] => {
+    return Array.from({length: count}, (v, k) => k).map(k => ({
+        id: `item-${k + offset}-${new Date().getTime()}`,
+        playerName: `player ${k + offset}`
+    }))
+};
+
+const reorder = (list: CandidatePlayer[], startIndex: number, endIndex: number) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+};
+
+/**
+ * Moves an item from one list to another list.
+ */
+const move = (source: any, destination: any, droppableSource: any, droppableDestination: any) => {
+    const sourceClone = Array.from(source);
+    const destClone = Array.from(destination);
+    const [removed] = sourceClone.splice(droppableSource.index, 1);
+
+    destClone.splice(droppableDestination.index, 0, removed);
+
+    const result = {};
+    // @ts-ignore
+    result[droppableSource.droppableId] = sourceClone;
+    // @ts-ignore
+    result[droppableDestination.droppableId] = destClone;
+
+    return result;
+};
+const grid = 8;
+
+const getItemStyle = (isDragging: boolean, draggableStyle: any) => ({
+    // some basic styles to make the items look a bit nicer
+    userSelect: "none",
+    padding: grid * 2,
+    margin: `0 0 ${grid}px 0`,
+
+    // change background colour if dragging
+    background: isDragging ? "lightgreen" : "grey",
+
+    // styles we need to apply on draggables
+    ...draggableStyle
+});
+const getListStyle = (isDraggingOver: boolean) => ({
+    background: isDraggingOver ? "lightblue" : "lightgrey",
+    padding: grid,
+    width: 250
+});
+
+
 const Teams: React.FunctionComponent<Props> = (props) => {
+    const [teams, setTeams] = useState([getCandidatePlayers(10), getCandidatePlayers(5, 10)]);
+
+
+    function onDragEnd(result: any) {
+        const {source, destination} = result;
+
+        // dropped outside the list
+        if (!destination) {
+            return;
+        }
+        const sInd = +source.droppableId;
+        const dInd = +destination.droppableId;
+
+        if (sInd === dInd) {
+            const items = reorder(teams[sInd], source.index, destination.index);
+            const newState = [...teams];
+            // @ts-ignore
+            newState[sInd] = items;
+            setTeams(newState);
+        } else {
+            const result = move(teams[sInd], teams[dInd], source, destination);
+            const newState = [...teams];
+            // @ts-ignore
+            newState[sInd] = result[sInd];
+            // @ts-ignore
+            newState[dInd] = result[dInd];
+
+            setTeams(newState.filter(group => group.length));
+        }
+    }
+
+
     return (
         <Container>
             <Breadcrumb>
@@ -16,44 +111,99 @@ const Teams: React.FunctionComponent<Props> = (props) => {
             <Row>
                 <Col>
                     <h1>Season 2020</h1>
+                </Col>
+                <Col>
                     <h2>{props.group}</h2>
                 </Col>
-            </Row>
-            <Row>
                 <Col>
-                    <h4>Lag #1</h4>
-                    <p>9-er</p>
-                </Col>
-                <Col>
-                    <h4>Lag #2</h4>
-                    <p>9-er</p>
-                </Col>
-                <Col>
-                    <h4>Lag #3</h4>
-                    <p>7-er</p>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <Button variant="dark">
-                        Save
+                    <Button className="m-1"
+                        type="button"
+                        onClick={() => {
+                            setTeams([...teams, []]);
+                        }}
+                    >
+                        Add team
                     </Button>
-                </Col>
-                <Col>
-                    <Button variant="dark">
-                        Save
-                    </Button>
-                </Col>
-                <Col>
-                    <Button variant="dark">
-                        Save
+                    <Button className="m-1"
+                        type="button"
+                        onClick={() => {
+                            setTeams([...teams, getCandidatePlayers(1)]);
+                        }}
+                    >
+                        Add new player
                     </Button>
                 </Col>
             </Row>
 
+            <Row>
+                <div>
+                </div>
+            </Row>
+            <Row>
+                <div>
+                    <div style={{display: "flex"}}>
+                        <DragDropContext onDragEnd={onDragEnd}>
+                            {teams.map((el, ind) => (
+                                <Droppable key={ind} droppableId={`${ind}`}>
+                                    {(provided, snapshot) => (
+                                        <div
+                                            ref={provided.innerRef}
+                                            style={getListStyle(snapshot.isDraggingOver)}
+                                            {...provided.droppableProps}
+                                        >
+                                            {el.map((item, index) => (
+                                                <Draggable
+                                                    key={item.id}
+                                                    draggableId={item.id}
+                                                    index={index}
+                                                >
+                                                    {(provided, snapshot) => (
+                                                        <div
+                                                            ref={provided.innerRef}
+                                                            {...provided.draggableProps}
+                                                            {...provided.dragHandleProps}
+                                                            style={getItemStyle(
+                                                                snapshot.isDragging,
+                                                                provided.draggableProps.style
+                                                            )}
+                                                        >
+                                                            <div
+                                                                style={{
+                                                                    display: "flex",
+                                                                    justifyContent: "space-around"
+                                                                }}
+                                                            >
+                                                                {item.playerName}
+                                                                <Button
+                                                                    type="button"
+                                                                    onClick={() => {
+                                                                        console.log(`playerId=${item.id}, playerName=${item.playerName}`);
+
+                                                                        const newState = [...teams];
+                                                                        newState[ind].splice(index, 1);
+                                                                        setTeams(
+                                                                            newState.filter(group => group.length)
+                                                                        );
+                                                                    }}
+                                                                >
+                                                                    remove
+                                                                </Button>
+                                                            </div>
+                                                        </div>
+                                                    )}
+                                                </Draggable>
+                                            ))}
+                                            {provided.placeholder}
+                                        </div>
+                                    )}
+                                </Droppable>
+                            ))}
+                        </DragDropContext>
+                    </div>
+                </div>
+            </Row>
         </Container>
     );
-
 };
-// <h1>Teams for {props.group}</h1>
+
 export default Teams;
